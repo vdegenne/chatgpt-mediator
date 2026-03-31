6cd6f67263b0fb1bd5bfb8878313ef1b8fde9b88
Valentin Degenne <vdegenne@gmail.com>
2026-03-31 21:50:05 +0200

    relative

--- src/app-shell/app-shell.ts
+++ src/app-shell/app-shell.ts
@@ -44,7 +44,7 @@ export class AppShell extends MaterialShellChild {
 					</md-icon-button>
 					<span slot="headline"
 						>${store.query
-							? html`<span class="text-3xl relative -top-1 text-bold" primary
+							? html`<span class="text-3xl text-bold" primary
 									>${store.query}</span
 								>`
 							: html`<b class="text-(--md-sys-color-error)"
